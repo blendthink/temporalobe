aec0c02be84f1926ca3084880ff57551ece2897a
blendthink <admin@blendthink.dev>
2022-07-12 16:11:33 +0900

    remove Fqdn, and refactor

--- lib/data/model/db/scheme.dart
+++ lib/data/model/db/scheme.dart
@@ -4,49 +4,15 @@ part 'scheme.g.dart';
 
 @Collection(accessor: 'categories')
 class Category {
-  int id = Isar.autoIncrement;
+  final id = Isar.autoIncrement;
 
   @Index(unique: true)
   late String name;
 }
 
-@Collection(accessor: 'fqdns')
-class Fqdn {
-  int id = Isar.autoIncrement;
-
-  final service = IsarLink<Service>();
-
-  late String fqdn;
-}
-
-@Collection(accessor: 'accounts')
-class Account {
-  int id = Isar.autoIncrement;
-
-  final service = IsarLink<Service>();
-
-  late final String username;
-  late String password;
-  late String totp;
-
-  @Backlink(to: 'account')
-  final fields = IsarLinks<Field>();
-}
-
-@Collection(accessor: 'fields')
-class Field {
-  int id = Isar.autoIncrement;
-
-  final account = IsarLink<Account>();
-
-  late bool canObscure;
-  late String key;
-  late String value;
-}
-
 @Collection(accessor: 'services')
 class Service {
-  int id = Isar.autoIncrement;
+  final id = Isar.autoIncrement;
 
   final category = IsarLink<Category>();
 
@@ -59,4 +25,29 @@ class Service {
 
   @Backlink(to: 'service')
   final accounts = IsarLinks<Account>();
+}
+
+@Collection(accessor: 'accounts')
+class Account {
+  final id = Isar.autoIncrement;
+
+  final service = IsarLink<Service>();
+
+  late String username;
+  late String password;
+  late String totp;
+
+  @Backlink(to: 'account')
+  final fields = IsarLinks<Field>();
+}
+
+@Collection(accessor: 'fields')
+class Field {
+  final id = Isar.autoIncrement;
+
+  final account = IsarLink<Account>();
+
+  late bool canObscure;
+  late String key;
+  late String value;
 }
