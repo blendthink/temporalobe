9eec4b72d46834736ebeb7ec2514736fe0fbdbcb
blendthink <admin@blendthink.dev>
2022-07-12 16:07:50 +0900

    change flutter_riverpod to hooks_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:temporalobe/data/model/db/scheme.dart';
--- lib/ui/services/index.dart
+++ lib/ui/services/index.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sticky_headers/sticky_headers.dart';
 import 'package:temporalobe/data/model/service/category.dart';
 import 'package:temporalobe/ui/services/component/service_tile.dart';
