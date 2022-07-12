5f4dae1dde76145b6be94859b99d7d33bf8625ff
blendthink <admin@blendthink.dev>
2022-07-12 16:03:43 +0900

    add isar init

---
+++ lib/data/store/isar.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+
+final isarProvider =
+    Provider<Isar>((ref) => throw Exception('Provider was not initialized'));
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:temporalobe/data/model/db/scheme.dart';
+import 'package:temporalobe/data/store/isar.dart';
 import 'package:temporalobe/ui/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationSupportDirectory();
+  final isar = await Isar.open(
+    schemas: [
+      AccountSchema,
+      CategorySchema,
+      FqdnSchema,
+      ServiceSchema,
+    ],
+    directory: dir.path,
+  );
+
   runApp(
-    const ProviderScope(
-      child: App(),
+    ProviderScope(
+      overrides: [
+        isarProvider.overrideWithValue(isar),
+      ],
+      child: const App(),
     ),
   );
 }
