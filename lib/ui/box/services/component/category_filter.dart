bebc2834f4fce5f2319106d1ea3d3496174b8eaa
blendthink <admin@blendthink.dev>
2022-07-13 18:46:48 +0900

    change from StickyHeader to ChoiceChip

--- lib/ui/box/services/component/category_filter.dart
+++ lib/ui/box/services/component/category_filter.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+const double _kListPadding = 8;
+const _kListGap = Gap(_kListPadding / 2);
+const double _kChipHeight = 32;
+const double _kListHeight = _kChipHeight + _kListPadding * 2;
 
 class CategoryFilter extends StatefulWidget {
   const CategoryFilter({
@@ -9,16 +15,38 @@ class CategoryFilter extends StatefulWidget {
 
   final List<String> categories;
 
-  final ValueChanged<String> onFilter;
+  final ValueChanged<String?> onFilter;
 
   @override
   State<StatefulWidget> createState() => _State();
 }
 
 class _State extends State<CategoryFilter> {
+  int? _selectedIndex;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return SizedBox(
+      height: _kListHeight,
+      child: ListView.separated(
+        padding: const EdgeInsets.all(_kListPadding),
+        itemCount: widget.categories.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final category = widget.categories[index];
+          return ChoiceChip(
+            label: Text(category),
+            selected: _selectedIndex == index,
+            onSelected: (selected) {
+              setState(() {
+                _selectedIndex = selected ? index : null;
+              });
+              widget.onFilter(selected ? category : null);
+            },
+          );
+        },
+        separatorBuilder: (context, index) => _kListGap,
+      ),
+    );
   }
 }
--- lib/ui/box/services/index.dart
+++ lib/ui/box/services/index.dart
@@ -1,7 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:sticky_headers/sticky_headers.dart';
-import 'package:temporalobe/data/model/service/category.dart';
+import 'package:temporalobe/ui/box/services/component/category_filter.dart';
 import 'package:temporalobe/ui/box/services/component/search_app_bar.dart';
 import 'package:temporalobe/ui/box/services/component/service_tile.dart';
 
@@ -13,120 +14,100 @@ class ServicesPage extends HookConsumerWidget {
     final t = Theme.of(context);
     return Scaffold(
       appBar: SearchAppBar(
-        onSearch: (text) {},
+        onSearch: (text) {
+          log(text);
+        },
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          final category = item.key;
-          final services = item.value;
-          return StickyHeader(
-            header: ColoredBox(
-              color: t.colorScheme.surfaceVariant,
-              child: ListTile(
-                title: Text(
-                  category.name,
-                  style: t.textTheme.titleSmall!.copyWith(
-                    color: t.colorScheme.onSurfaceVariant,
-                  ),
-                ),
-              ),
-            ),
-            content: ListView.builder(
-              shrinkWrap: true,
-              physics: const ClampingScrollPhysics(),
+      body: Column(
+        children: [
+          CategoryFilter(
+            onFilter: (category) {
+              log(category.toString());
+            },
+            categories: const [
+              'Work',
+              'Shopping',
+              'SNS',
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
               itemCount: services.length,
               itemBuilder: (context, index) {
                 final service = services[index];
                 return ServiceTile(service: service);
               },
             ),
-          );
-        },
+          )
+        ],
       ),
     );
   }
 }
 
-const items = <MapEntry<ServiceCategory, List<ServiceItem>>>[
-  MapEntry(
-    ServiceCategory(name: 'Work'),
-    <ServiceItem>[
-      ServiceItem(
-        title: 'Google',
-        fqdn: 'google.com',
-      ),
-      ServiceItem(
-        title: 'Apple',
-        fqdn: 'apple.com',
-      ),
-      ServiceItem(
-        title: 'Microsoft',
-        fqdn: 'microsoft.com',
-      ),
-      ServiceItem(
-        title: 'freee',
-        fqdn: 'freee.co.jp',
-      ),
-    ],
+const services = <ServiceItem>[
+  ServiceItem(
+    title: 'Google',
+    fqdn: 'google.com',
   ),
-  MapEntry(
-    ServiceCategory(name: 'Shopping'),
-    <ServiceItem>[
-      ServiceItem(
-        title: 'Amazon',
-        fqdn: 'amazon.co.jp',
-      ),
-      ServiceItem(
-        title: 'Shopify',
-        fqdn: 'shopify.com',
-      ),
-      ServiceItem(
-        title: 'Yahoo!',
-        fqdn: 'yahoo.co.jp',
-      ),
-      ServiceItem(
-        title: 'mercari',
-        fqdn: 'jp.mercari.com',
-      ),
-      ServiceItem(
-        title: '楽天',
-        fqdn: 'www.rakuten.co.jp',
-      ),
-    ],
+  ServiceItem(
+    title: 'Apple',
+    fqdn: 'apple.com',
   ),
-  MapEntry(
-    ServiceCategory(name: 'SNS'),
-    <ServiceItem>[
-      ServiceItem(
-        title: 'Twitter',
-        fqdn: 'twitter.com',
-      ),
-      ServiceItem(
-        title: 'LINE',
-        fqdn: 'line.me',
-      ),
-      ServiceItem(
-        title: 'GitHub',
-        fqdn: 'github.com',
-      ),
-      ServiceItem(
-        title: 'Facebook',
-        fqdn: 'facebook.com',
-      ),
-      ServiceItem(
-        title: 'Instagram',
-        fqdn: 'instagram.com',
-      ),
-      ServiceItem(
-        title: 'Tiktok',
-        fqdn: 'tiktok.com',
-      ),
-      ServiceItem(
-        title: 'YouTube',
-        fqdn: 'youtube.com',
-      ),
-    ],
+  ServiceItem(
+    title: 'Microsoft',
+    fqdn: 'microsoft.com',
+  ),
+  ServiceItem(
+    title: 'freee',
+    fqdn: 'freee.co.jp',
+  ),
+  ServiceItem(
+    title: 'Amazon',
+    fqdn: 'amazon.co.jp',
+  ),
+  ServiceItem(
+    title: 'Shopify',
+    fqdn: 'shopify.com',
+  ),
+  ServiceItem(
+    title: 'Yahoo!',
+    fqdn: 'yahoo.co.jp',
+  ),
+  ServiceItem(
+    title: 'mercari',
+    fqdn: 'jp.mercari.com',
+  ),
+  ServiceItem(
+    title: '楽天',
+    fqdn: 'www.rakuten.co.jp',
+  ),
+  ServiceItem(
+    title: 'Twitter',
+    fqdn: 'twitter.com',
+  ),
+  ServiceItem(
+    title: 'LINE',
+    fqdn: 'line.me',
+  ),
+  ServiceItem(
+    title: 'GitHub',
+    fqdn: 'github.com',
+  ),
+  ServiceItem(
+    title: 'Facebook',
+    fqdn: 'facebook.com',
+  ),
+  ServiceItem(
+    title: 'Instagram',
+    fqdn: 'instagram.com',
+  ),
+  ServiceItem(
+    title: 'Tiktok',
+    fqdn: 'tiktok.com',
+  ),
+  ServiceItem(
+    title: 'YouTube',
+    fqdn: 'youtube.com',
   ),
 ];
